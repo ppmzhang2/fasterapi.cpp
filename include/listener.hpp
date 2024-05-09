26371e4c8b40133f2750e09a632799db6e1c793c
ZHANG, Meng <stall-breezes.0t@icloud.com>
2024-05-09 15:43:18 +1200

    Revision
    - replace `Server` with `Listener` class
    - catch acceptor error
    - simplify imports

---
+++ include/listener.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <asio.hpp>
+#include <stdint.h>
+
+namespace Server {
+
+    class Listener {
+      public:
+        // Constructor to initialize the Listener object with the specified
+        // IO context and port.
+        Listener(const uint16_t port);
+
+        // Start listening for incoming connections on the specified port.
+        asio::awaitable<void> Start();
+
+      private:
+        // The port on which the server listens for incoming connections.
+        const uint16_t port_;
+
+      private:
+        // Handle a single client connection.
+        asio::awaitable<void> session(asio::ip::tcp::socket socket);
+    };
+
+} // namespace Server
